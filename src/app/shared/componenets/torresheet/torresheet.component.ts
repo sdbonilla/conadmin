e0f0304355fe9b43a6bec2f26f14df3bf1ca0663
sdbonilla <santiagobonilla@unicomfacauca.edu.co>
2021-03-17 14:01:08 -0500

    adding_firebase

--- src/app/shared/componenets/torresheet/torresheet.component.ts
+++ src/app/shared/componenets/torresheet/torresheet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-torresheet',
@@ -7,8 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TorresheetComponent implements OnInit {
 
-  constructor() { }
+  constructor(public actionSheetController: ActionSheetController) {}
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
-  ngOnInit() {}
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 
 }
